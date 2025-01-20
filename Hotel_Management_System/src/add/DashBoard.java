7ab743cb4046b7b011ee7ceb4548b856e8f21294
Onim Hassan <168928238+Onim19@users.noreply.github.com>
2025-01-20 12:51:34 +0600

    Update DashBoard.java

--- Hotel_Management_System/src/add/DashBoard.java
+++ Hotel_Management_System/src/add/DashBoard.java
@@ -3,54 +3,96 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.print.Book;
 
-public class HotelManagementSystem extends JFrame implements ActionListener{
-    public HotelManagementSystem(){
-		//frame creating
-        //basic background setting
+public class DashBoard extends JFrame implements ActionListener {
+    JMenuItem addEmployee,addRoom,addReciption;
+    JButton BookRoom,Employees,Rooms;
+    public DashBoard(){
         super.setSize(1280,720);
         super.setLocation(150,50);
         super.setLayout(null);
-        ImageIcon icon=new ImageIcon("E:\\Ownproject\\Java Project\\Hotel_Management_System\\img\\Icon.png");//Icon
-        ImageIcon image_background=new ImageIcon("E:\\Ownproject\\Java Project\\Hotel_Management_System\\img\\Background.png");//image for background
-        super.setResizable(false);
         super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        super.setIconImage(icon.getImage());
-        super.setTitle("American International Hotel Service");//adding title for the project
-        JLabel L_Background=new JLabel(image_background);//label1 for adding image on background
-        L_Background.setBounds(0,0,1280,720);
-        super.add(L_Background);
-        //adding a panel for title name
-        JPanel t_bg=new JPanel();
-        t_bg.setBounds(120,10,1000,80);
-        t_bg.setBackground(Color.WHITE);
-        L_Background.add(t_bg);
-        //adding another label
-        JLabel L_Title=new JLabel("American International Hotel-Bangladesh");
-        L_Title.setBounds(110,0,1000,80);
-        L_Title.setForeground(Color.BLACK);
-        L_Title.setFont(new Font("Serif", Font.BOLD, 50));
-        L_Title.setBackground(Color.BLACK);
-        t_bg.add(L_Title);
-     
-         //Button adding for entering the next page
-        JButton next_button=new JButton("NEXT");
-        next_button.setBounds(1000,600,150,50);
-        next_button.setFocusable(false);
-        L_Background.add(next_button);
-        next_button.setForeground(Color.BLACK);
-        next_button.setBackground(Color.white);
-        next_button.setFont(new Font("Serif", Font.BOLD, 15));
-        next_button.addActionListener(this);
+        super.setResizable(false);
+        ImageIcon icon=new ImageIcon("E:\\Ownproject\\Java Project\\Hotel_Management_System\\img\\Icon.png");//Icon
+        ImageIcon dashboard_image=new ImageIcon("E:\\Ownproject\\Java Project\\Hotel_Management_System\\img\\dashboard.png");//dashboard background
+        Image D_image1=dashboard_image.getImage().getScaledInstance(1280,720,Image.SCALE_DEFAULT);
+        ImageIcon D_image2=new ImageIcon(D_image1);
+        JLabel d_image=new JLabel(D_image2);
+        d_image.setBounds(0,0,1280,720);
+        super.setIconImage(icon.getImage());//adding icon
+        super.add(d_image);
+        super.setTitle("Dashboard");
+
+        //adding a welcome text message
+        JLabel welcome_TEXT=new JLabel("Welcome");
+        welcome_TEXT.setBounds(420,30,400,90);
+        welcome_TEXT.setFont(new Font("Serif",Font.BOLD,100));
+        welcome_TEXT.setForeground(Color.BLACK);
+        d_image.add(welcome_TEXT);
+
+        //creating a menubar
+        JMenuBar mb=new JMenuBar();
+        mb.setBounds(0,0,1280,25);
+        d_image.add(mb);
+        //adding menu
+        JMenu menu1=new JMenu("HOTEL MANAGEMENT");
+        menu1.setForeground(Color.BLUE);
+        mb.add(menu1);
+        //adding menu Item for menu1
+        addReciption=new JMenuItem("Reciption");
+        menu1.add(addReciption);
+
+        JMenu menu2=new JMenu("ADMIN");
+        menu2.setForeground(Color.RED);
+        mb.add(menu2);
+        //adding menu item for menu2
+
+        addRoom=new JMenuItem("Add Room");
+        addRoom.addActionListener(this);
+        menu2.add(addRoom);
+
+        addEmployee=new JMenuItem("Add Employee");
+        addEmployee.addActionListener(this);
+        menu2.add(addEmployee);
+
+        //adding  buttons for bookroom,employee details,roomdetails
+        BookRoom=new JButton("Book Room");
+        BookRoom.setBounds(1000,150,200,50);
+        BookRoom.setBackground(Color.WHITE);
+        BookRoom.setForeground(Color.BLACK);
+        BookRoom.setFocusable(false);
+        BookRoom.setFont(new Font("TimesRoman",Font.BOLD,20));
+        d_image.add(BookRoom);
+
+        Employees=new JButton("Employee Details");
+        Employees.setBounds(1000,220,200,50);
+        Employees.setBackground(Color.WHITE);
+        Employees.setForeground(Color.BLACK);
+        Employees.setFocusable(false);
+        Employees.setFont(new Font("TimesRoman",Font.BOLD,20));
+        d_image.add(Employees);
+
+        Rooms=new JButton("Room Details");
+        Rooms.setBounds(1000,290,200,50);
+        Rooms.setBackground(Color.WHITE);
+        Rooms.setForeground(Color.BLACK);
+        Rooms.setFocusable(false);
+        Rooms.setFont(new Font("TimesRoman",Font.BOLD,20));
+        d_image.add(Rooms);
 
         super.setVisible(true);
     }
-   public void actionPerformed(ActionEvent ae){
-       super.setVisible(false);
-	   new Login();
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource()==addEmployee){
+            new AddEmployee();
+        }
+        else if(e.getSource()==addRoom){
+            new AddRoom();
+        }
     }
 
     public static void main(String[] args) {
-      new HotelManagementSystem();
+        new DashBoard();
     }
 }
